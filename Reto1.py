5795e7524e0b5d1cd6222e88149f8dc7a459ae18
Juan José Tobón Villa <jujotovil@gmail.com>
2021-07-05 21:40:54 -0500

    Update Reto1.py

--- Reto1.py
+++ Reto1.py
@@ -1,21 +1,17 @@
-poder = int(input("Escribe el nivel de poder de Luffy: "))
-
-def luffy(nivel=poder):
+def luffy(nivel):
   fase1 = nivel
   fase2 = (nivel * 2) + 4
   fase3 = (fase2 + fase1)//5
   print(fase1, fase2, fase3)
 
-  def gear(gear=fase3):
-    if(0 <= fase3 <= 20):
-      print("Uno")
-    elif(21 <= fase3 <= 30):
-      print("Dos")
-    elif(31 <= fase3 <= 50):
-      print("Tres")
-    else:
-      print("Cuatro")
-    return print(gear())
-  return gear()
+  if(0 <= fase3 <= 20):
+    print("Uno")
+  elif(21 <= fase3 <= 30):
+    print("Dos")
+  elif(31 <= fase3 <= 50):
+    print("Tres")
+  else:
+    print("Cuatro")
+  return 
 
-(luffy())
+luffy(int(input("Escribe el nivel de poder de Luffy: ")))
