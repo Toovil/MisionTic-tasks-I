6d2c5c866e785fe457a69a98f08b09b235352658
Juan José Tobón Villa <jujotovil@gmail.com>
2021-07-07 00:21:16 -0500

    Cambios reto 4

--- reto4.py
+++ reto4.py
@@ -1,9 +1,18 @@
 import json
 
-def SOFAH(cantidad, personajes):
-    personajes = json.load(personajes)
-    cantidad = cantidad.split()
+def reto(personajes,lista):
+      lista1=0
+      lista2=[]
 
-    salida_cantidad = 0
-    salida_personajes = []
-    
+      personajes = json.loads(personajes)
+      lista = lista.split()
+      for buy in lista:
+        for key in personajes.keys():
+         
+          if buy == key:
+              lista1+=personajes[key]
+              lista2.append(buy)
+            
+      return (print(lista1),"\n" , print(" ".join(lista2)))
+
+reto(input("Personajes: "),input("Lista: "))
